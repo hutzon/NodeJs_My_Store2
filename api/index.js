89b77a8c638d1fe608bf80d8ac2d999d5942898b
Ottoniel Campos <hutzon4011@gmail.com>
2024-01-17 21:31:48 -0600

    deploy

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+const whitelist = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, (err, res) => {
+  console.log('Mi port ' + port);
+});
